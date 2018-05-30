d98dc83ad404e457e4f4324f69551a0edda16331
Huggable <771304257@qq.com>
2018-05-30 11:09:06 +0800

    1108

--- model/ft.py
+++ model/ft.py
@@ -10,121 +10,113 @@ from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import SGD
+sys.path.append("../")
 
-train_dir = 'D:/data/5/train'  # 训练集数据
-val_dir = 'D:/data/5/test' # 验证集数据
-IM_WIDTH, IM_HEIGHT = 299, 299 #fixed size for InceptionV3
-NB_EPOCHS = 3
-BAT_SIZE = 32
-FC_SIZE = 1024
-NB_IV3_LAYERS_TO_FREEZE = 172
-lr = 0.0001
+from model.Config import configInc
 
 
 def get_nb_files(directory):
-  """Get number of files by searching directory recursively"""
-  if not os.path.exists(directory):
-    return 0
-  cnt = 0
-  for r, dirs, files in os.walk(directory):
-    for dr in dirs:
-      cnt += len(glob.glob(os.path.join(r, dr + "/*")))
-  return cnt
+    if not os.path.exists(directory):
+        return 0
+    cnt = 0
+    for r, dirs, files in os.walk(directory):
+        for dr in dirs:
+            cnt += len(glob.glob(os.path.join(r, dr + "/*")))
+    return cnt
 
 
 def setup_to_transfer_learn(model, base_model):
-  """Freeze all layers and compile the model"""
-  for layer in base_model.layers:
-    layer.trainable = False
-  model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    for layer in base_model.layers:
+        layer.trainable = False
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
 
 def add_new_last_layer(base_model, nb_classes):
-  """Add last layer to the convnet
-  Args:
+    """Add last layer to the convnet
+    Args:
     base_model: keras model excluding top
     nb_classes: # of classes
-  Returns:
+    Returns:
     new keras model with last layer
-  """
-  x = base_model.output
-  x = GlobalAveragePooling2D()(x)
-  x = Dense(FC_SIZE, activation='relu')(x) #new FC layer, random init
-  predictions = Dense(nb_classes, activation='softmax')(x) #new softmax layer
-  model = Model(input=base_model.input, output=predictions)
-  return model
+    """
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(configInc.FC_SIZE, activation='relu')(x) #new FC layer, random init
+    predictions = Dense(nb_classes, activation='softmax')(x) #new softmax layer
+    model = Model(input=base_model.input, output=predictions)
+    return model
 
 
 def setup_to_finetune(model):
-  """Freeze the bottom NB_IV3_LAYERS and retrain the remaining top layers.
-  note: NB_IV3_LAYERS corresponds to the top 2 inception blocks in the inceptionv3 arch
-  Args:
+    """Freeze the bottom NB_IV3_LAYERS and retrain the remaining top layers.
+    note: NB_IV3_LAYERS corresponds to the top 2 inception blocks in the inceptionv3 arch
+    Args:
     model: keras model
-  """
-  for layer in model.layers[:NB_IV3_LAYERS_TO_FREEZE]:
-     layer.trainable = False
-  for layer in model.layers[NB_IV3_LAYERS_TO_FREEZE:]:
-     layer.trainable = True
-  model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+    """
+    for layer in model.layers[:configInc.NB_IV3_LAYERS_TO_FREEZE]:
+        layer.trainable = False
+    for layer in model.layers[configInc.NB_IV3_LAYERS_TO_FREEZE:]:
+        layer.trainable = True
+    model.compile(optimizer=SGD(lr=configInc.lr, momentum=configInc.momentum), loss='categorical_crossentropy', metrics=['accuracy'])
 
 def plot_training(history):
-  acc = history.history['acc']
-  val_acc = history.history['val_acc']
-  loss = history.history['loss']
-  val_loss = history.history['val_loss']
-  epochs = range(len(acc))
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(len(acc))
 
-  plt.plot(epochs, acc, 'r.')
-  plt.plot(epochs, val_acc, 'r')
-  plt.title('Training and validation accuracy')
+    plt.plot(epochs, acc, 'r.')
+    plt.plot(epochs, val_acc, 'r')
+    plt.title('Training and validation accuracy')
 
-  plt.figure()
-  plt.plot(epochs, loss, 'r.')
-  plt.plot(epochs, val_loss, 'r-')
-  plt.title('Training and validation loss')
-  plt.show()
+    plt.figure()
+    plt.plot(epochs, loss, 'r.')
+    plt.plot(epochs, val_loss, 'r-')
+    plt.title('Training and validation loss')
+    plt.show()
 
 
 
 
 """Use transfer learning and fine-tuning to train a network on a new dataset"""
-nb_train_samples = get_nb_files(train_dir)
-nb_classes = len(glob.glob(train_dir + "/*"))
-nb_val_samples = get_nb_files(val_dir)
-nb_epoch = int(NB_EPOCHS)
-batch_size = int(BAT_SIZE)
+nb_train_samples = get_nb_files(configInc.train_dir)
+nb_classes = len(glob.glob(configInc.train_dir + "/*"))
+nb_val_samples = get_nb_files(configInc.val_dir)
+nb_epoch = int(configInc.NB_EPOCHS)
+batch_size = int(configInc.BAT_SIZE)
 
 
 
 # data prep
 train_datagen =  ImageDataGenerator(
-  preprocessing_function=preprocess_input,
-  rotation_range=30,
-  width_shift_range=0.2,
-  height_shift_range=0.2,
-  shear_range=0.2,
-  zoom_range=0.2,
-  horizontal_flip=True
+preprocessing_function=preprocess_input,
+rotation_range=30,
+width_shift_range=0.2,
+height_shift_range=0.2,
+shear_range=0.2,
+zoom_range=0.2,
+horizontal_flip=True
 )
 test_datagen = ImageDataGenerator(
-  preprocessing_function=preprocess_input,
-  rotation_range=30,
-  width_shift_range=0.2,
-  height_shift_range=0.2,
-  shear_range=0.2,
-  zoom_range=0.2,
-  horizontal_flip=True
+preprocessing_function=preprocess_input,
+rotation_range=30,
+width_shift_range=0.2,
+height_shift_range=0.2,
+shear_range=0.2,
+zoom_range=0.2,
+horizontal_flip=True
 )
 
 train_generator = train_datagen.flow_from_directory(
-train_dir,
-target_size=(IM_WIDTH, IM_HEIGHT),
+configInc.train_dir,
+target_size=(configInc.IM_WIDTH, configInc.IM_HEIGHT),
 batch_size=batch_size,
 )
 
 validation_generator = test_datagen.flow_from_directory(
-val_dir,
-target_size=(IM_WIDTH, IM_HEIGHT),
+configInc.val_dir,
+target_size=(configInc.IM_WIDTH, configInc.IM_HEIGHT),
 batch_size=batch_size,
 )
 
@@ -154,14 +146,11 @@ validation_data=validation_generator,
 nb_val_samples=nb_val_samples,
 class_weight='auto')
 
-model.save("../inception.h5")
+model.save(configInc.inception_model_save_dir)
 
 
 
 
-
-
-
-
-plot_training(history_ft)
+if configInc.draw:
+    plot_training(history_ft)
 
